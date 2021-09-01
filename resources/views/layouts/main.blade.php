0152e49815bb1d60c14bbd4fc26a72e8a7625f45
Lays Barbosa <barbosalays36@gmail.com>
2021-08-31 21:38:57 -0300

    Criação do projeto e layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--Fontawesome-->
+    <script src="https://kit.fontawesome.com/f889f8f087.js" crossorigin="anonymous"></script>
+
+    <!-- MEU CSS -->
+    <link rel="stylesheet" href="{{asset('/css/estilo.css')}}"
+
+    <title>Projeto Album</title>
+  </head>
+  <body>
+
+    @yield('content')
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
